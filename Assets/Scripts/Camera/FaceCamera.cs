b5a3313f5784aa6300d8c7bc4b5db51950438e48
William Hammond <william.t.hammond@gmail.com>
2022-05-17 09:37:05 -0400

    Setups up physics layers, rotates health component towards camera

---
+++ Assets/Scripts/Camera/FaceCamera.cs
@@ -0,0 +1,19 @@
+using UnityEngine;
+
+public class FaceCamera : MonoBehaviour
+{
+    private Transform mainCameraTransform;
+
+    private void Start()
+    {
+        mainCameraTransform = Camera.main.transform;
+    }
+
+    private void LateUpdate()
+    {
+        transform.LookAt(
+            transform.position + mainCameraTransform.rotation * Vector3.forward,
+            mainCameraTransform.rotation * Vector3.up
+        );
+    }
+}
