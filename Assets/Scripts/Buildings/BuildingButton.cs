82577f747f3fa9dfd6cd7ec8d8369b8878f5ab6f
William Hammond <william.t.hammond@gmail.com>
2022-06-16 00:20:13 -0400

    Moves BuildingButton to the correct namespace

--- Assets/Scripts/Buildings/BuildingButton.cs
+++ Assets/Scripts/Buildings/BuildingButton.cs
@@ -1,111 +1,110 @@
-using System;
-using Buildings;
 using Mirror;
-using Mirror.Examples.Chat;
 using Networking;
 using TMPro;
 using UnityEngine;
 using UnityEngine.EventSystems;
 using UnityEngine.InputSystem;
-using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
-public class BuildingButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
+namespace Buildings
 {
-    [SerializeField]
-    private Building _building;
-
-    [SerializeField]
-    private Image iconImage;
-
-    [SerializeField]
-    private TMP_Text priceText;
-
-    [SerializeField]
-    private LayerMask floorMask;
-
-    private Camera mainCamera;
-    private RTSPlayer player;
-    private GameObject buildingPreviewInstance;
-    private Renderer buildingRendererInstance;
-    private BoxCollider buildingCollider;
-
-    private static readonly int BaseColor = Shader.PropertyToID("_BaseColor");
-
-    private void Start()
+    public class BuildingButton : MonoBehaviour, IPointerDownHandler, IPointerUpHandler
     {
-        mainCamera = Camera.main;
+        [SerializeField]
+        private Building _building;
 
-        iconImage.sprite = _building.GetIcon();
-        priceText.text = _building.GetPrice().ToString();
+        [SerializeField]
+        private Image iconImage;
 
-        player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
+        [SerializeField]
+        private TMP_Text priceText;
 
-        buildingCollider = _building.GetComponent<BoxCollider>();
-    }
+        [SerializeField]
+        private LayerMask floorMask;
 
-    private void Update()
-    {
-        if (buildingPreviewInstance)
+        private Camera mainCamera;
+        private RTSPlayer player;
+        private GameObject buildingPreviewInstance;
+        private Renderer buildingRendererInstance;
+        private BoxCollider buildingCollider;
+
+        private static readonly int BaseColor = Shader.PropertyToID("_BaseColor");
+
+        private void Start()
         {
-            UpdateBuildingPreview();
-        }
-    }
+            mainCamera = Camera.main;
 
-    public void OnPointerDown(PointerEventData eventData)
-    {
-        if (eventData.button != PointerEventData.InputButton.Left)
-        {
-            return;
-        }
+            iconImage.sprite = _building.GetIcon();
+            priceText.text = _building.GetPrice().ToString();
 
-        if (player.GetResources() < _building.GetPrice())
-        {
-            return;
-        }
+            player = NetworkClient.connection.identity.GetComponent<RTSPlayer>();
 
-        buildingPreviewInstance = Instantiate(_building.GetBuildingPreview());
-        buildingRendererInstance = buildingPreviewInstance.GetComponentInChildren<Renderer>();
-
-        buildingPreviewInstance.SetActive(false);
-    }
-
-    public void OnPointerUp(PointerEventData eventData)
-    {
-        if (buildingPreviewInstance == null)
-        {
-            return;
+            buildingCollider = _building.GetComponent<BoxCollider>();
         }
 
-        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-
-        if (Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, floorMask))
+        private void Update()
         {
-            player.CmdTryPlaceBuilding(_building.GetId(), hit.point);
+            if (buildingPreviewInstance)
+            {
+                UpdateBuildingPreview();
+            }
         }
 
-        Destroy(buildingPreviewInstance);
-    }
-
-    private void UpdateBuildingPreview()
-    {
-        Ray ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
-
-        if (!Physics.Raycast(ray, out RaycastHit hit, Mathf.Infinity, floorMask))
+        public void OnPointerDown(PointerEventData eventData)
         {
-            return;
+            if (eventData.button != PointerEventData.InputButton.Left)
+            {
+                return;
+            }
+
+            if (player.GetResources() < _building.GetPrice())
+            {
+                return;
+            }
+
+            buildingPreviewInstance = Instantiate(_building.GetBuildingPreview());
+            buildingRendererInstance = buildingPreviewInstance.GetComponentInChildren<Renderer>();
+
+            buildingPreviewInstance.SetActive(false);
         }
 
-        buildingPreviewInstance.transform.position = hit.point;
-
-        if (!buildingPreviewInstance.activeSelf)
+        public void OnPointerUp(PointerEventData eventData)
         {
-            buildingPreviewInstance.SetActive(true);
+            if (buildingPreviewInstance == null)
+            {
+                return;
+            }
+
+            var ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if (Physics.Raycast(ray, out var hit, Mathf.Infinity, floorMask))
+            {
+                player.CmdTryPlaceBuilding(_building.GetId(), hit.point);
+            }
+
+            Destroy(buildingPreviewInstance);
         }
 
-        Color color = player.CanPlaceBuilding(buildingCollider, hit.point)
-          ? Color.green
-          : Color.red;
-        buildingRendererInstance.material.SetColor(BaseColor, color);
+        private void UpdateBuildingPreview()
+        {
+            var ray = mainCamera.ScreenPointToRay(Mouse.current.position.ReadValue());
+
+            if (!Physics.Raycast(ray, out var hit, Mathf.Infinity, floorMask))
+            {
+                return;
+            }
+
+            buildingPreviewInstance.transform.position = hit.point;
+
+            if (!buildingPreviewInstance.activeSelf)
+            {
+                buildingPreviewInstance.SetActive(true);
+            }
+
+            var color = player.CanPlaceBuilding(buildingCollider, hit.point)
+              ? Color.green
+              : Color.red;
+            buildingRendererInstance.material.SetColor(BaseColor, color);
+        }
     }
 }
